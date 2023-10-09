635800ce579148a49f94ac266b6ff0d19013635e
Beveky <im.beveky@gmail.com>
2023-10-09 21:29:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+////////////////////// Exercice1/////////////////////////////
+num1 = 2;
+num2 = 5;
+test1 = num1 % 2;
+test2 = num2 % 2;
+result1 = test1 == 0;
+result2 = test2 == 0;
+
+console.log(num1, "is an even number?", result1);
+console.log(num2, "is an even number?", result2);
+////////////////////// Exercice2/////////////////////////////
+var now = "Now in ";
+var three = 3;
+var d = "D!";
+console.log(now + three + d);
+////////////////////// Exercice3/////////////////////////////
+var counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter);
